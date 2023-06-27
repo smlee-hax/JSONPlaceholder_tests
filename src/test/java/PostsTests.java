535665d7e188541909815bd8a80919016258ae1c
Shelby <slee.nums.1337@gmail.com>
2023-06-27 10:26:15 -0700

    init commit

---
+++ src/test/java/PostsTests.java
@@ -0,0 +1,27 @@
+//explicit imports to avoid bloat
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PostsTests {
+
+
+    //POST
+
+    //Success post (user, body, title)
+
+    //Duplicate posts by same user (valid)
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(true, true);
+    }
+
+
+
+    //Assumption: is there a post limit. Some social media websites limit number of posts per minute to reduce spam
+    //hit that post limit here
+
+
+    //GET
+
+}
